f081f7dac5b80b1b60472d6806f8313eed5b0963
Sharon Mwenda <smkathambi@gmail.com>
2022-06-13 13:29:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+let newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = 'Sharon is the champion';
+
