7849ae4781ed2af5c1934e0d7ac528fe04ca958a
Sharon Mwenda <smkathambi@gmail.com>
2022-06-21 15:20:16 +0300

    add query selector

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Write your code here!
 
-main.remove();
-
+//main.remove();
+document.querySelector("#main").remove()
 let newHeader = document.createElement("h1");
-document.body.append(newHeader);
 newHeader.id = "victory";
 newHeader.innerHTML = 'Sharon is the champion';
 
